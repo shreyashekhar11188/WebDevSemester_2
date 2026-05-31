600a4eda4e53d6d89bbf6357d9053987110b50c2
shreya <shreya.25b01011188@abes.ac.in>
2026-05-31 15:53:07 +0530

    exp

---
+++ JSfolder/script.js
@@ -0,0 +1,32 @@
+// console.log("helloworld");
+// let a=10;
+// console.log(a)
+
+
+// var a=10;
+// var a=20;
+// console.log(a);
+// {
+//     var b=30;
+//     console.log(b);
+//     console.log(typeof b);
+// }
+// console.log(b);
+
+
+
+// {
+//     let b=40;
+//     console.log(b);
+// }
+// console.log(b);
+
+
+// {
+//     const name="Monika";
+//     name="singh";
+    
+// }
+// console.log(name);
+
+
